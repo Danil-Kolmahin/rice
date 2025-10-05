d94cb748d722c78c4d396a3b5af82ea31d6498a8
Danil Kolmahin <danil.kolmahin@gmail.com>
2025-10-05 09:37:08 +0000

    feat: setup improvements

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-uv run ansible-playbook setup.yml --ask-become-pass
+uv run ansible-playbook -K -i localhost, -e 'target_hosts=localhost' -c local setup.yml
