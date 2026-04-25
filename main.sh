4d5b105d5727615424c0a34abb0181b1ff1eb84d
Danil Kolmahin <danil.kolmahin@gmail.com>
2026-04-25 10:52:52 +0300

    docs: add comment to main.sh

--- main.sh
+++ main.sh
@@ -7,6 +7,6 @@ uv run ansible-playbook -K -i localhost, -e 'target_hosts=localhost' -c local se
 
 # if device==pc then set FRAME='tower' and run with tags -base -develop -loaf -extended -experiments
 # if device==laptop then set FRAME='cart' and run with tags -base -develop -loaf
-# if device==phone then set FRAME='pony' and run with tags -base -phone
+# if device==phone then set FRAME='shield' and run with tags -base -phone
 # if device==vm then set FRAME='mirror' and run with tags -base -vm -server
 # if device==server then set FRAME='outpost' and run with tags -base -server -experiments
