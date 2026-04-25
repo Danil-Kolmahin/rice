cf6a43ad88a32f0c2834fa4a4dd9722d51057c9e
Danil Kolmahin <danil.kolmahin@gmail.com>
2026-04-25 10:25:49 +0300

    docs: add init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# determine device==?
+
+# run one time os configuration
+
+# show whiptail to query secret data (username, hostname, password, disc encryption password, maybe ask for pass and get from there?)
