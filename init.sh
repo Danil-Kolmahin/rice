682dd2cce9936fbdfcc9c5c6ec4ae8ed2d566628
Danil Kolmahin <danil.kolmahin@gmail.com>
2026-06-06 10:12:50 +0300

    feat: apply script WIP

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# determine device==?
-
-# run one time os configuration
-
-# show whiptail to query secret data (username, hostname, password, disc encryption password, maybe ask for pass and get from there?)
--- prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-uv sync
-uv run ansible-galaxy collection install -r requirements.yml
