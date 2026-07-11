7f6680683d141d0e3cc1f3405a41c81e42a0cc13
Danil Kolmahin <danil.kolmahin@gmail.com>
2026-07-11 11:19:50 +0300

    feat: add lua LSP to nvim

--- roles/base/files/.config/nvim/init.lua
+++ roles/base/files/.config/nvim/init.lua
@@ -7,3 +7,27 @@ vim.cmd([[
   highlight LineNr guibg=none ctermbg=none
   highlight SignColumn guibg=none ctermbg=none
 ]])
+
+vim.o.autocomplete = true
+vim.opt.complete:append('o')
+vim.opt.completeopt = { 'menuone', 'noselect' }
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
+
+vim.pack.add{
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+}
+vim.lsp.enable('lua_ls')
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+    if client:supports_method('textDocument/completion') then
+      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+      -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+      -- client.server_capabilities.completionProvider.triggerCharacters = chars
+
+      vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
+    end
+  end,
+})
