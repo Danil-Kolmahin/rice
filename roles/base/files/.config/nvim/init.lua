d08199b4a470cae22f52891fa3ea2bea0abf7fa2
Danil Kolmahin <danil.kolmahin@gmail.com>
2026-09-18 11:46:20 +0300

    feat: replace nvim file explorer with fern

--- roles/base/files/.config/nvim/init.lua
+++ roles/base/files/.config/nvim/init.lua
@@ -15,7 +15,7 @@ vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
 
 vim.pack.add{
   { src = 'https://github.com/neovim/nvim-lspconfig' },
-  { src = 'https://github.com/nvim-tree/nvim-tree.lua' }, -- TODO: remove and switch to ranger integration if possible
+  { src = 'https://github.com/lambdalisue/vim-fern' },  -- TODO: remove and switch to ranger integration if possible
   { src = 'https://github.com/mhinz/vim-startify' },
 }
 
@@ -48,15 +48,6 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   end,
 })
 
-require("nvim-tree").setup{
-  renderer = { icons = { show = {
-    git = false,
-    folder = false,
-    file = false,
-    folder_arrow = false,
-  } } }
-}
-
 vim.g.startify_lists = { -- Only show Sessions and Recent files
   { type = 'sessions',  header = { 'Sessions' } },
   { type = 'files',     header = { 'Recent files' } },
@@ -65,10 +56,3 @@ vim.g.startify_custom_header = {} -- Remove header text
 vim.g.startify_session_number = 5 -- Limit recent sessions count globally
 vim.g.startify_files_number = 5 -- Limit recent files count globally
 vim.g.startify_enable_special = 0 -- Disable the standard [q] quit entry at the bottom
-vim.g.startify_session_before_save = { 'NvimTreeClose' } -- Exclude nvim-tree buffers from session files
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'StartifyBufferOpened',
-  callback = function()
-    require('nvim-tree.api').tree.open()
-  end,
-})
